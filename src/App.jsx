ffdc106b2ede17e3c5626bc329c5a8dd3941d301
Darsh D. Coding <darshdcoding@gmail.com>
2025-11-10 13:34:26 +0530

    integrating shadcn/ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
+import {Button} from '@/components/ui/button'
 import './App.css'
 
 function App() {
-
   return (
     <>
         <h1 className="text-3xl font-bold underline">
-            Hello world!
+            Radhe Radhe...
         </h1>
     </>
   )
