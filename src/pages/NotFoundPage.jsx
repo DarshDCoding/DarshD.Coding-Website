2832e2f6a15e2a40238a53e93d57a81b44eccd63
Darsh D. Coding <darshdcoding@gmail.com>
2025-11-12 00:50:51 +0530

    modifying PageNotFound page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -5,12 +5,12 @@ export default function PageNotFound(){
 
     return (
         <>
-            <div className="h-[100vh] w-[100vw] flex flex-col justify-center items-center gap-8">
-                <div className="flex flex-col justify-center items-center gap-2">
-                <h1 className="font-decorative font-extrabold text-6xl">404</h1>
-                <h2 className="font-decorative font-extrabold text-2xl">Page Not Found.</h2>
+            <div className="h-[100vh] w-[100vw] flex flex-col justify-center items-center">
+                <div className="relative flex flex-col justify-center items-center gap-2">
+                <h1 className="font-primary font-extrabold text-[14rem]">404</h1>
+                <h2 className="absolute font-decorative font-extrabold text-[1.2rem] -rotate-60 bg-gray-50">Page Not Found.</h2>
                 </div>
-                <NavLink to="/" className="font-decorative"><Button>Home Page</Button></NavLink>
+                <NavLink to="/" className="font-decorative -mt-12 "><Button>Home Page</Button></NavLink>
             </div>
         </>
     )
