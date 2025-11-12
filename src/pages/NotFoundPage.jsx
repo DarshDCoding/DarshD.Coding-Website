a55008a9a572e7b42fdc5f23e13d697babe6ac1d
Darsh D. Coding <darshdcoding@gmail.com>
2025-11-12 08:49:32 +0530

    modifying PageNotFound page for Mobile devices

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -7,10 +7,10 @@ export default function PageNotFound(){
         <>
             <div className="h-[100vh] w-[100vw] flex flex-col justify-center items-center">
                 <div className="relative flex flex-col justify-center items-center gap-2">
-                <h1 className="font-primary font-extrabold text-[14rem]">404</h1>
-                <h2 className="absolute font-decorative font-extrabold text-[1.2rem] -rotate-60 bg-gray-50">Page Not Found.</h2>
+                <h1 className="font-primary font-extrabold text-[10rem] md:text-[14rem] z-10">404</h1>
+                <h2 className="absolute font-decorative font-extrabold text-[0.82rem] md:text-[1.12rem] -rotate-60 bg-gray-50 z-10 ">Page Not Found.</h2>
                 </div>
-                <NavLink to="/" className="font-decorative -mt-12 "><Button>Home Page</Button></NavLink>
+                <NavLink to="/" className="font-decorative -mt-5 md:-mt-12 z-30"><Button>Home Page</Button></NavLink>
             </div>
         </>
     )
